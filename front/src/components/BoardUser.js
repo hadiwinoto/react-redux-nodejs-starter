57836bdcb2ff022acc26dfb1637536abae8cfc96
hadiwinoto <inimulyohadiwinoto@gmail.com>
2022-08-12 16:19:02 +0700

    update

--- front/src/components/BoardUser.js
+++ front/src/components/BoardUser.js
@@ -30,8 +30,29 @@ const BoardUser = () => {
 
   return (
     <div className="container">
-      <div className="card">
-        <span>{content}</span>
+      <div className="row">
+          <div className="col-md-3">
+              <div className="card">
+                <div class="card-header px-3"><b>Database</b></div>
+                <ul class="list-group list-group-light list-group-small">
+                  <li class="list-group-item px-3 border-0 active" aria-current="true">
+                     Komersial
+                  </li>
+                  <li class="list-group-item px-3">Industri</li>
+                  <li class="list-group-item px-3">Agri</li>
+                </ul>
+              </div>
+          </div>
+          <div className="col-md-9">
+              <div className="card">
+                <div class="card-header px-3"><b>List Database Komersial</b></div>
+                <ul class="list-group list-group-light list-group-small">
+                  <li class="list-group-item px-3">Apartment</li>
+                  <li class="list-group-item px-3">Resort</li>
+                  <li class="list-group-item px-3">Hotel</li>
+                </ul>
+              </div>
+          </div>
       </div>
     </div>
   );
